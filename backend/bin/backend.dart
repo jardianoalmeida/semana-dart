e08f6085007b01e885701e2f47ab5c9b574a3d0f
Jardiano <jardiano.almeida@gmail.com>
2022-07-24 19:55:33 -0300

    fix: Explication  Middleware

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,11 +1,31 @@
+import 'dart:async';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
-  final server = await io.serve((request) {
-    print(request);
-    return Response(200, body: 'Corpo');
-  }, '0.0.0.0', 4466);
+  var pipeline = Pipeline()..addMiddleware(log());
+  final server = await io.serve(pipeline.addHandler(_handle), '0.0.0.0', 4466);
 
   print('Online -  ${server.address.address}:${server.port}');
+}
+
+Middleware log() {
+  return (handle) {
+    return (request) async {
+      // antes de executar
+      print('solicitado: ${request.url}');
+      final response = await handle(request);
+      //depois de executar
+      print('[${response.statusCode}] - Response');
+
+      return response;
+    };
+  };
+}
+
+FutureOr<Response> _handle(Request request) {
+  print(request);
+
+  return Response(200, body: 'Corpo');
 }
