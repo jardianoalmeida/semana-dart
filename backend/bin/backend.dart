99265edd03ebb9c4884648968a938e99fd13a559
Jardiano <jardiano.almeida@gmail.com>
2022-07-24 19:44:41 -0300

    fix: Primary contact shelf io

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,5 +1,11 @@
-import 'package:backend/backend.dart' as backend;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${backend.calculate()}!');
+void main(List<String> arguments) async {
+  final server = await io.serve((request) {
+    print(request);
+    return Response(200, body: 'Corpo');
+  }, '0.0.0.0', 4466);
+
+  print('Online -  ${server.address.address}:${server.port}');
 }
